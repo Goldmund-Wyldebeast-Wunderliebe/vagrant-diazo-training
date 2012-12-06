8996dfd54a24daa2d02ff8d167e9fa1b61c5e390
Kim Chee Leong <kc@leong.nl>
2012-12-06 11:38:28 +0100

    Updated Plone set-up, adding Django project

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,9 @@
 from fabric.api import env, local, run, cd
 from utils import list_dir
 
-plone_installer_url = 'https://launchpad.net/plone/4.2/4.2.1/+download/Plone-4.2.1-UnifiedInstaller.tgz'
-gw20e_buildout = 'git://github.com/Goldmund-Wyldebeast-Wunderliebe/gw20e.buildout'
+plone_buildout = 'https://github.com/kcleong/buildout'
 diazo_plone = 'git://github.com/Goldmund-Wyldebeast-Wunderliebe/diazo-training-plone.git'
+diazo_django = 'git://github.com/Goldmund-Wyldebeast-Wunderliebe/diazo-training-django.git'
 
 
 def vagrant():
@@ -38,36 +38,33 @@ def install_base():
     run('sudo apt-get -y install libxslt1-dev')
     run('sudo apt-get -y install libxml2-dev')
     run('sudo apt-get -y install git-core')
+    run('sudo apt-get -y install vim')
 
 
 def install_plone():
-
-    plone_dir = 'plone-4.2.1'
-    if plone_dir not in list_dir():
-        run('wget {0}'.format(plone_installer_url))
-        run('tar -zxf Plone-4.2.1-UnifiedInstaller.tgz')
-        run('rm Plone-4.2.1-UnifiedInstaller.tgz')
-        run('mv Plone-4.2.1-UnifiedInstaller {0}'.format(plone_dir))
-
-    with cd(plone_dir):
-        run('./install.sh standalone')
-
-
-def install_plone_gw20e():
     
     plone_dir = 'plone'
+
     if plone_dir not in list_dir():
-        run('git clone {0}'.format(gw20e_buildout))
+        run('git clone {0} {1}'.format(plone_buildout, plone_dir))
 
     with cd(plone_dir):
-        # First get prerequisites
-        run('git clone {0} diazo'.format(diazo_plone))
-        run('wget http://cobain.gw20e.com/leong/local-diazo.cfg -O local.cfg')
-        run('wget http://cobain.gw20e.com/leong/gw20e.buildout-eggs.tgz')
-        run('tar -zxf gw20e.buildout-eggs.tgz && rm gw20e.buildout-eggs.tgz')
+        # Get prerequisites
+        if 'training' not in list_dir():
+            run('git clone {0} training'.format(diazo_plone))
+        run('wget http://cobain.gw20e.com/leong/plone-4.3a-eggs.tgz')
+        run('tar -zxf plone-4.3a-eggs.tgz && rm plone-4.3a-eggs.tgz')
+
         # Set-up buildout
         run('virtualenv .')
-        run('./bin/python bootstrap.py -c local.cfg')
-        run('./bin/buildout -c diazo/local.cfg')
-        run('./bin/instance run diazo/structure.py')
+        run('./bin/python bootstrap.py')
+        run('./bin/buildout')
+        run('./bin/instance run training/structure.py')
         run('./bin/instance start')
+
+def install_django():
+    django_dir = 'django'
+
+    if django_dir not in list_dir():
+        run('git clone {0} {1}'.format(django_buildout, django_dir))
+
